d7123dd821caf8e6af9fb52d56f91e3817460ba5
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-03 21:56:53 +0100

    testing

---
+++ scripts/score.gd
@@ -0,0 +1,21 @@
+# score.gd
+extends Node
+
+signal gold_change(amount: int)
+
+var _gold: int = 50
+
+
+func add_gold(amount: int) -> void:
+	_gold += amount
+	gold_change.emit(_gold)
+	
+func substract_gold(amount: int) -> void:
+	_gold -= amount
+	gold_change.emit(_gold)
+
+func _ready():
+	pass
+	
+func _process(delta: float) -> void:
+	pass
