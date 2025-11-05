4c57c8f9d0a982a9886db64e2c83a8d7fbd362bb
Angelet-code <sagara.ad@gmail.com>
2025-11-05 19:23:23 +0100

    coins

--- scripts/score.gd
+++ scripts/score.gd
@@ -2,11 +2,13 @@
 extends Node
 
 signal gold_change(amount: int)
+@onready var win_coins: AudioStreamPlayer2D = $Win_Coins
 
 var gold: int = 50
 
 func add_gold(amount: int) -> void:
 	gold += amount
+	win_coins.play()
 	gold_change.emit(gold)
 	
 func substract_gold(amount: int) -> void:
