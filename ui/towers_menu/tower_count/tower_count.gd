99971ab95ef3ef5ae5f674f9632cc7b239f5cd34
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-15 23:28:03 +0100

    tower count ui

---
+++ ui/towers_menu/tower_count/tower_count.gd
@@ -0,0 +1,55 @@
+@tool
+class_name TowerCount extends Control
+
+const TIER_1_COUNT = 2
+const TIER_2_COUNT = 4
+const TIER_3_COUNT = 6
+const TIER_4_COUNT = 8
+
+
+@export var icon: Texture2D:
+	set(value):
+		icon = value
+		_update_texture()
+
+var count: int = 0:
+	set = _set_count
+	
+var max: int = TIER_1_COUNT:
+	set(value):
+		max = value
+		max_label.text = str(max)
+
+@onready var current_label: Label = $RedCount/Value/CurrentLabel
+@onready var texture_rect: TextureRect = $RedCount/TextureRect
+
+@onready var max_label: Label = $RedCount/Value/MaxLabel
+
+func _ready() -> void:
+	_update_texture()
+	texture_rect.texture = icon
+
+func _update_texture():
+	if texture_rect:
+		texture_rect.texture = icon
+
+func _set_count(value: int) -> void:
+	match value:
+		var x when x < TIER_1_COUNT:
+			max = TIER_1_COUNT
+			pass
+		var x when x >= TIER_1_COUNT and x < TIER_2_COUNT:
+			max = TIER_2_COUNT
+			pass
+		var x when x >= TIER_2_COUNT and x < TIER_3_COUNT:
+			max = TIER_3_COUNT
+			pass
+		var x when x >= TIER_3_COUNT and x < TIER_4_COUNT:
+			max = TIER_4_COUNT
+			pass
+		var x when x > TIER_4_COUNT:
+			pass
+		
+	count = value
+	if count <= TIER_4_COUNT:
+		current_label.text = str(count)
--- ui/towers_menu/towers_menu.gd
+++ ui/towers_menu/towers_menu.gd
@@ -8,8 +8,12 @@ signal tower_selected(tower_scene: PackedScene)
 
 @export var tower_placer: TowerPlacer
 
+@onready var red_tower_count: TowerCount = $TowersCount/RedTowerCount
+@onready var green_tower_count: TowerCount = $TowersCount/GreenTowerCount
+@onready var blue_tower_count: TowerCount = $TowersCount/BlueTowerCount
+
 func _ready():
-	pass
+	tower_placer.tower_placed.connect(_on_tower_placed)
 	
 func _on_red_tower_button_pressed() -> void:
 	if Score.gold >= Price.TowerBuild.RED:
@@ -24,4 +28,7 @@ func _on_green_tower_button_pressed() -> void:
 		tower_selected.emit(GREEN_TOWER)
 
 func _on_tower_placed(tower_type: Tower.TowerType, amount: int) -> void:
-	pass
+	match tower_type:
+		Tower.TowerType.RED: red_tower_count.count = amount
+		Tower.TowerType.GREEN: green_tower_count.count = amount
+		Tower.TowerType.BLUE: blue_tower_count.count = amount
