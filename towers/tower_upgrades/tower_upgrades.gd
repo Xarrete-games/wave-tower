3cc2d43cb7beca5a2bf2bd617e500694df11a865
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-16 02:35:14 +0100

    upgrades structure

--- towers/tower_upgrades/tower_upgrades.gd
+++ towers/tower_upgrades/tower_upgrades.gd
@@ -1,9 +1,16 @@
 extends Node
 
-@export var tower_placer: TowerPlacer
-
-func _ready() -> void:
-	tower_placer.towers_placed.connect(_on_tower_placed)
-
-func _on_tower_placed(tower_type: Tower.TowerType, amount: int) -> void:
+func on_tower_placed(tower_type: Tower.TowerType, amount: int) -> void:
+	match tower_type:
+		Tower.TowerType.RED: _hand_red_updates
+		Tower.TowerType.GREEN: _hand_green_updates
+		Tower.TowerType.BLUE: _hand_blue_updates
+		
+func _hand_red_updates(amount: int) -> void:
+	pass
+	
+func _hand_green_updates(amount: int) -> void:
+	pass
+	
+func _hand_blue_updates(amount: int) -> void:
 	pass
