6c98f10afbf0b8aab1f242278060bf66c2759dad
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-16 17:02:13 +0100

    show card

--- ui/rewards_ui/reward_card/reward_card.gd
+++ ui/rewards_ui/reward_card/reward_card.gd
@@ -4,9 +4,15 @@ signal card_pressed(relic: Relic)
 
 var relic: Relic
 
+@onready var description: RichTextLabel = $MarginContainer/MarginContainer/VBoxContainer/Description
+@onready var title: Label = $MarginContainer/MarginContainer/VBoxContainer/Title
+@onready var texture_button: TextureButton = $TextureButton
+
 func set_relic(new_relic_value: Relic) -> void:
 	relic = new_relic_value
-	pass
+	texture_button.texture_normal = relic.get_texture()
+	title.text = relic.get_id()
+	description.text = relic.get_description()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
