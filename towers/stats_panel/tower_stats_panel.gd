0454262819658ad336415c13d8347244fbe3611e
Angelet-code <sagara.ad@gmail.com>
2025-11-19 01:22:39 +0100

    hover tower

--- towers/stats_panel/tower_stats_panel.gd
+++ towers/stats_panel/tower_stats_panel.gd
@@ -4,9 +4,9 @@ class_name TowerStatsPanel extends Control
 @onready var attack_speed_label: Label = $VBoxContainer/HBoxContainer2/AttackSpeedLabel
 @onready var range_label: Label = $VBoxContainer/HBoxContainer3/RangeLabel
 @onready var critic_label: Label = $VBoxContainer/HBoxContainer4/CriticLabel
-
+	
 func update_stats(tower_stats: TowerStats) -> void:
-	damage_label.text = str(tower_stats.damage)
+	damage_label.text = str(int(tower_stats.damage))
 	attack_speed_label.text = str(tower_stats.attack_speed)
-	range_label.text = str(tower_stats.attack_range)
-	critic_label.text = str(tower_stats.critic_chance)
+	range_label.text = str(int(tower_stats.attack_range))
+	critic_label.text = str(int(tower_stats.critic_chance))+"%"
