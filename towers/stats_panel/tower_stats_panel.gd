159707e3f121f6cb5518b575a08cd047bb07330d
Angelet-code <sagara.ad@gmail.com>
2025-11-19 03:41:11 +0100

    tower stats panel turbofixed
    Zoom independent
    Aestetics
    text corrections

--- towers/stats_panel/tower_stats_panel.gd
+++ towers/stats_panel/tower_stats_panel.gd
@@ -1,11 +1,9 @@
 class_name TowerStatsPanel extends CanvasLayer
 
-@onready var damage_label: Label = $PanelContainer/VBoxContainer/HBoxContainer/DamageLabel
-@onready var attack_speed_label: Label = $PanelContainer/VBoxContainer/HBoxContainer2/AttackSpeedLabel
-@onready var range_label: Label = $PanelContainer/VBoxContainer/HBoxContainer3/RangeLabel
-@onready var critic_label: Label = $PanelContainer/VBoxContainer/HBoxContainer4/CriticLabel
-
-
+@onready var damage_label: Label = $PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/DamageLabel
+@onready var attack_speed_label: Label = $PanelContainer/VBoxContainer/HBoxContainer2/VBoxContainer/AttackSpeedLabel
+@onready var range_label: Label = $PanelContainer/VBoxContainer/HBoxContainer3/VBoxContainer/RangeLabel
+@onready var critic_label: Label = $PanelContainer/VBoxContainer/HBoxContainer4/VBoxContainer2/CriticLabel
 
 func update_stats(tower_stats: TowerStats) -> void:
 	damage_label.text = str(int(tower_stats.damage))
