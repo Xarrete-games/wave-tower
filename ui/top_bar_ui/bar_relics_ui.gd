0768bc9a48111b7778b57e0d42906002e3703114
Angelet-code <sagara.ad@gmail.com>
2025-11-21 00:09:05 +0100

    top bar fix

---
+++ ui/top_bar_ui/bar_relics_ui.gd
@@ -0,0 +1,19 @@
+class_name RelicsBar extends Control
+
+const TOP_BAR_RELIC = preload("uid://f34dinc60kaa")
+
+func _ready():
+	RelicsManager.relics_change.connect(_update_relics)
+
+func _update_relics(relics: Array):
+	clear_relics_container()
+	for relic in relics:
+		var relic_instance = TOP_BAR_RELIC.instantiate()
+		add_child(relic_instance)
+		relic_instance.get_texture().texture = relic.get_texture()
+		if relic.amount > 1:
+			relic_instance.get_amount().text = str(relic.amount)
+
+func clear_relics_container() -> void:
+	for relic in get_children():
+		relic.queue_free()
