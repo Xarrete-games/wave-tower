aeb6d922816daa0f152c72b6a44746669e349870
Angelet-code <sagara.ad@gmail.com>
2025-11-23 02:33:38 +0100

    rewards to chetas

--- ui/rewards_ui/reward_card/reward_card.gd
+++ ui/rewards_ui/reward_card/reward_card.gd
@@ -2,13 +2,16 @@ class_name Rewardard extends Control
 
 signal card_pressed(relic: Relic)
 
+const COMMON_COLOR = Color.GREEN_YELLOW
+const RARE_COLOR = Color.DODGER_BLUE
+const EPIC_COLOR = Color.GOLD
 var relic: Relic
 
 @onready var description: RichTextLabel = $VBoxContainer/Description
 @onready var title: Label = $Title
 @onready var amount_gold_label: Label = $VBoxContainer/Gold/AmountGoldLabel
-
 @onready var relic_texture: TextureRect = $RelicTexture
+@onready var hexagon_border: Polygon2D = $Control/HexagonBorder
 
 func set_relic(new_relic_value: Relic) -> void:
 	relic = new_relic_value
@@ -16,6 +19,12 @@ func set_relic(new_relic_value: Relic) -> void:
 	title.text = relic.id
 	description.text = relic.description
 	amount_gold_label.text = str(relic.price)
+	if relic.rarity == Relic.RelicRarity.COMMON:
+		hexagon_border.color = COMMON_COLOR
+	if relic.rarity == Relic.RelicRarity.RARE:
+		hexagon_border.color = RARE_COLOR
+	if relic.rarity == Relic.RelicRarity.EPIC:
+		hexagon_border.color = EPIC_COLOR
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
