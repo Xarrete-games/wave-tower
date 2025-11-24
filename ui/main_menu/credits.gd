0958d7f05ead6ee90ab4b852fa7ba3fb4e6079cc
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-25 00:30:47 +0100

    main menu

---
+++ ui/main_menu/credits.gd
@@ -0,0 +1,4 @@
+class_name Credits extends CanvasLayer
+
+func _on_exit_button_pressed() -> void:
+	queue_free()
--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,6 +1,7 @@
 class_name MainMenu extends Control
 
 const GAME = preload("uid://6vgrx5dct8h8")
+const CREDITS = preload("uid://bayb10jsajj4a")
 
 @export var direct_init: bool = true
 
@@ -11,10 +12,8 @@ func _ready() -> void:
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_credits_button_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().root.add_child(CREDITS.instantiate())
 
 func _on_new_run_button_pressed() -> void:
 	get_tree().change_scene_to_packed(GAME)
