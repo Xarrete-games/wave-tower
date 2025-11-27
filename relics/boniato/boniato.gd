d9900df825c31a809342e90393ad4dcd897f4bdf
Angelet-code <sagara.ad@gmail.com>
2025-11-27 12:37:05 +0100

    nerf boniato wtf

--- relics/boniato/boniato.gd
+++ relics/boniato/boniato.gd
@@ -4,7 +4,7 @@ const BONIATO_DATA = preload("uid://uxa8ct8ljcw2")
 
 func apply_effect() -> void:
 	Score.extra_gold_dropped = Score.extra_gold_dropped + 1
-	LiveManager.lives -= 2
+	LiveManager.lives -= 5
 
 func get_data() -> RelicData:
 	return BONIATO_DATA
