9ab1525b4d7a67bf2f460ff868ff7215d612044d
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-29 00:17:44 +0100

    tower descriptions

--- ui/towers_menu/tower_button/tower_button.gd
+++ ui/towers_menu/tower_button/tower_button.gd
@@ -13,6 +13,12 @@ signal pressed(tower_scene: PackedScene)
 		icon_hover = value
 		_update_texture_hover()
 
+@export_multiline var tower_description: String:
+	set(value):
+		tower_description = value
+		if hint_label:
+			hint_label.text = value
+
 var price: int = 0:
 	set(value):
 		price = value
@@ -20,10 +26,16 @@ var price: int = 0:
 
 @onready var tower_button: TextureButton = $VBoxContainer/CenterContainer/TowerButton
 @onready var gold_price: GoldPrice = $VBoxContainer/GoldPrice
+@onready var hint_container: PanelContainer = $HintContainer
+@onready var hint_label: Label = $HintContainer/HintLabel
+
 
 func _ready() -> void:
 	_update_texture()
-
+	hint_container.visible = false
+	if hint_label:
+		hint_label.text = tower_description
+	
 func _update_texture():
 	if tower_button:
 		tower_button.texture_normal = icon
@@ -36,3 +48,10 @@ func _on_tower_button_pressed() -> void:
 	if Score.gold < price:
 		return
 	pressed.emit(tower_scene)
+
+func _on_tower_button_mouse_entered() -> void:
+	hint_container.visible = true
+
+
+func _on_tower_button_mouse_exited() -> void:
+	hint_container.visible = false
