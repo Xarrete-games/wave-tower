0f639b9e619aef526847ad406cab1cb7e78f61f5
CarlosribesDev <carlosribes.dev@gmail.com>
2025-11-30 19:15:01 +0100

    block reward card boniato

--- ui/rewards_ui/reward_card/reward_card.gd
+++ ui/rewards_ui/reward_card/reward_card.gd
@@ -15,15 +15,20 @@ var relic_colors: Dictionary[Relic.RelicRarity, Color] = {
 	Relic.RelicRarity.EPIC: EPIC_COLOR,
 }
 var _has_enough_gold = false
+var _has_enough_live = false
+var _is_boniato = false
 
 @onready var description: RichTextLabel = $VBoxContainer/Description
 @onready var title: Label = $Title
 @onready var amount_gold_label: Label = $VBoxContainer/Gold/AmountGoldLabel
 @onready var relic_texture: TextureRect = $RelicTexture
 @onready var hexagon_border: Polygon2D = $Control/HexagonBorder
+@onready var amount_live_label: Label = $VBoxContainer/LivePriceContainer/AmountLiveLabel
+@onready var live_price_container: HBoxContainer = $VBoxContainer/LivePriceContainer
 
 func _ready() -> void:
 	Score.gold_change.connect(_check_price)
+	live_price_container.visible = false
 
 func set_relic(new_relic_value: Relic) -> void:
 	relic = new_relic_value
@@ -34,14 +39,27 @@ func set_relic(new_relic_value: Relic) -> void:
 	hexagon_border.color =  relic_colors[relic.rarity]
 	price = relic.price
 	_check_price(Score.gold)
-
+	if relic is Boniato:
+		live_price_container.visible = true
+		_is_boniato = true
+		_chek_live(LiveManager.lives)
+		LiveManager.lives_change.connect(_chek_live)
+	
 func _on_gui_input(event: InputEvent) -> void:
-	if not _has_enough_gold:
+	if not _has_enough_gold or (_is_boniato and not _has_enough_live):
 		return
 
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
 			card_pressed.emit(relic)
+
+func _chek_live(curren_live: int) -> void:
+	_has_enough_live = curren_live > 5
+	amount_live_label.label_settings = (
+		LABEL_SETTINGS_24 
+		if _has_enough_live
+		else LABEL_SETTINGS_24_INVALID
+	)
 
 func _check_price(current_gold: int) -> void:
 	_has_enough_gold = current_gold >= price
@@ -51,7 +69,6 @@ func _check_price(current_gold: int) -> void:
 		else LABEL_SETTINGS_24_INVALID
 	)
 	
-
 func _on_mouse_entered() -> void:
 	relic_texture.scale = Vector2(0.6, 0.6)
 
