684e8074c64989a808f4cb14e2c078270416326e
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-03 21:35:57 +0100

    burn area

--- levels/kill_event_handler.gd
+++ levels/kill_event_handler.gd
@@ -1,9 +1,18 @@
 class_name KillEventHandler extends Node
 
+@export var burn_area_scene: PackedScene
+
 @onready var enemy_generator: EnemyGenerator = %EnemyGenerator
 
 func _ready() -> void:
 	enemy_generator.enemy_killed.connect(_on_enemy_killed)
 
 func _on_enemy_killed(enemy: Enemy, attack: Attack) -> void:
-	pass
+	if attack.source is WildFireTower:
+		_spawn_burn_area(enemy.global_position)
+
+func _spawn_burn_area(position: Vector2) -> void:
+	var burn_area: BurnArea = burn_area_scene.instantiate()
+	add_child(burn_area)
+	burn_area.global_position = position
+	burn_area.setup()
---
+++ towers/instances/fire/fire_wildfire_tower/burn_area.gd
@@ -0,0 +1,23 @@
+class_name BurnArea extends Area2D
+
+var enemies_burned: Array[Enemy] = []
+
+@onready var duration_timer: Timer = $DurationTimer
+
+func _ready() -> void:
+	monitoring = false
+
+func setup() -> void:
+	duration_timer.start()
+	monitoring = true
+
+func _on_body_entered(body: Node2D) -> void:
+	var enemy = body as Enemy
+	if enemies_burned.has(enemy):
+		return
+	enemy.apply_debuff(RunContext.enemy_debuff.get_debuff(EnemyDebuff.Type.BURN))
+	enemies_burned.append(enemy)
+
+
+func _on_duration_timer_timeout() -> void:
+	queue_free()
