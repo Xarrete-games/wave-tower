fe886c793219d38dc68b735a0ec84d264a789fbf
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-06 20:10:48 +0100

    aa

--- towers/instances/lightning/lighning_wave_tower/ligthning_wave_tower.gd
+++ towers/instances/lightning/lighning_wave_tower/ligthning_wave_tower.gd
@@ -25,7 +25,7 @@ func _fire_projectil(projectil: GreenProjectile) -> void:
 	if not _current_target:
 		return
 	
-	crystal_light.play()
+	cristal_light.play()
 	
 	const START_OFFSET_PERCENTAGE: float = 0.4
 	
