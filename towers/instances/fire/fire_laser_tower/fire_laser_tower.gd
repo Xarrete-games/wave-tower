686fbcf1892ec4e594aa8023221ab33f6d8f2bc1
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-08 17:59:09 +0100

    fix laser

--- towers/instances/fire/fire_laser_tower/fire_laser_tower.gd
+++ towers/instances/fire/fire_laser_tower/fire_laser_tower.gd
@@ -22,10 +22,9 @@ func _on_extra_stats_change(extra_stats: TowerExtraStats) -> void:
 
 func _fire() -> void:
 	var next_attack = _get_attack() if _current_target.get_percentage_remaining_health() > execute_threshold else _get_letal_attack()
+
 	var debuff = RunContext.enemy_debuff.get_debuff(EnemyDebuff.Type.BURN) if apply_burn else null
 	red_projectile.set_target(_current_target, next_attack, debuff)
-	
-	red_projectile.set_attack(next_attack)
 	red_projectile.hit_target()
 	cristal_light.turn_on()
 
