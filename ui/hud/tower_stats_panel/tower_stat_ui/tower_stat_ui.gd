a56320e8e8ba4af1313341a3cfcc2653c6e55248
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-24 01:28:03 +0100

    float stat

--- ui/hud/tower_stats_panel/tower_stat_ui/tower_stat_ui.gd
+++ ui/hud/tower_stats_panel/tower_stat_ui/tower_stat_ui.gd
@@ -3,6 +3,7 @@ class_name TowerStatUi extends Control
 @export var stat_name: String
 @export var stat_icon: Texture2D
 @export var stat_value: float
+@export var is_float: bool = false
 
 
 @onready var stat_texture: TextureRect = %StatTexture
@@ -12,8 +13,14 @@ class_name TowerStatUi extends Control
 func _ready() -> void:
 	stat_texture.texture = stat_icon
 	attibute_label.text = stat_name
-	value_label.text = str(int(stat_value))
+	if is_float:
+		value_label.text = str(stat_value)
+	else:
+		value_label.text = str(int(stat_value))
 
 func set_value(new_value: float) -> void:
 	stat_value = new_value
-	value_label.text = str(int(stat_value))
+	if is_float:
+		value_label.text = str(stat_value)
+	else:
+		value_label.text = str(int(stat_value))
