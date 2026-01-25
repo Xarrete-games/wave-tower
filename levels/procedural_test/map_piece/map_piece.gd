7f674616843269eb5730c1adf79fccdfcfcf2131
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-25 17:42:38 +0100

    get coords

---
+++ levels/procedural_test/map_piece/map_piece.gd
@@ -0,0 +1,40 @@
+class_name MapPiece extends TileMapLayer
+
+enum Dir { NE, SE, SW, NW }
+
+@export var edges_directions: Array[Dir] = []
+
+var edges_tiles: Array[Vector2i] = []	
+
+func _ready() -> void:
+	for edge in edges_directions:
+		var edge_center = get_edge_center(edge)
+		edges_tiles.append(edge_center)
+
+func get_edge_center(dir: Dir) -> Vector2i:
+	var used: Array[Vector2i] = get_used_cells()
+	var edge: Array[Vector2i] = []
+
+	match dir:
+		Dir.NE:
+			var min_y = used.map(func(c): return c.y).min()
+			edge = used.filter(func(c): return c.y == min_y)
+
+		Dir.SW:
+			var max_y = used.map(func(c): return c.y).max()
+			edge = used.filter(func(c): return c.y == max_y)
+
+		Dir.NW:
+			var min_x = used.map(func(c): return c.x).min()
+			edge = used.filter(func(c): return c.x == min_x)
+
+		Dir.SE:
+			var max_x = used.map(func(c): return c.x).max()
+			edge = used.filter(func(c): return c.x == max_x)
+
+	# get center of edge
+	var sum: Vector2i = Vector2i.ZERO
+	for c in edge:
+		sum += c
+
+	return sum / edge.size()
