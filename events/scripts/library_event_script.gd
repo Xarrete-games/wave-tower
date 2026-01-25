623ae0303422e08031b5fc84f85981c5c3ab7a91
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-25 03:56:33 +0100

    library event

---
+++ events/scripts/library_event_script.gd
@@ -0,0 +1,19 @@
+class_name LLibraryEventScript extends EventScript
+
+func get_options() -> Array[EventOptionData]:
+	var tome_relics = DataLoader.get_not_used_relics().filter(func(relic_data: RelicItemData):
+		return relic_data.is_tome and not RunContext.relics_manager.is_maxed(relic_data.id)
+	)
+
+	var options: Array[EventOptionData] = []
+	for relic_data in tome_relics:
+		var option: EventOptionData = EventOptionData.new("Acquire the %s" % relic_data.display_name, relic_data)
+		options.append(option)
+
+	return options
+
+func handle_response(data: Variant) -> void:
+	var relic_data: RelicItemData = data as RelicItemData
+	if relic_data:
+		var relic: Relic = relic_data.create_item() as Relic
+		RunContext.relics_manager.add_relic(relic)
--- items/relics/relic.gd
+++ items/relics/relic.gd
@@ -6,6 +6,7 @@ enum Rarity { COMMON, RARE, EPIC, ALL }
 var rarity: Rarity
 var max_stacks: int = 1
 var is_cursed: bool = false
+var is_tome: bool = false
 var disabled: bool = false
 
 func _init(data: RelicItemData) -> void:
@@ -13,6 +14,7 @@ func _init(data: RelicItemData) -> void:
 	rarity = data.rarity
 	max_stacks = data.max_stacks
 	is_cursed = data.is_cursed
+	is_tome = data.is_tome
 
 @abstract
 func apply_effect() -> void
