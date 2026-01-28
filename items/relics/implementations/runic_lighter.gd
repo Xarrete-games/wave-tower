24ae7abdf7cc075d1906996969a5068c9b66a4c3
Angelet-code <sagara.ad@gmail.com>
2026-01-28 12:52:47 +0100

    Relic fixes

--- items/relics/implementations/runic_lighter.gd
+++ items/relics/implementations/runic_lighter.gd
@@ -1,11 +1,11 @@
 class_name RunicLighter extends Relic
 
 const SOURCE_ID = "runic_lighter"
-const DAMAGE_INCREASE_PER_TOWER: float = 0.2
+const DAMAGE_INCREASE_PER_TOWER: float = 0.1
 
 func apply_effect() -> void:
-    var modifier = BurnDamageByTowersModifier.new(DamageTakenModifier.SourceType.RELIC, SOURCE_ID, DAMAGE_INCREASE_PER_TOWER)
-    RunContext.enemy_debuff_manager.add_modifier(modifier)
+	var modifier = BurnDamageByTowersModifier.new(DamageTakenModifier.SourceType.RELIC, SOURCE_ID, DAMAGE_INCREASE_PER_TOWER)
+	RunContext.enemy_debuff_manager.add_modifier(modifier)
 
 func remove_effect() -> void:
-    RunContext.enemy_debuff_manager.remove_modifier(SOURCE_ID)
+	RunContext.enemy_debuff_manager.remove_modifier(SOURCE_ID)
