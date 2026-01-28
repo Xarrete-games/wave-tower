1913c1b8396a74e363af212994ca1a0e69f682f1
Angelet-code <sagara.ad@gmail.com>
2026-01-28 13:22:21 +0100

    metronomo fix

--- items/relics/implementations/metronome.gd
+++ items/relics/implementations/metronome.gd
@@ -3,7 +3,7 @@ class_name Metronome extends Relic
 const SOURCE_ID = "metronome"
 
 func apply_effect() -> void:
-	var tower_buff = TowerBuff.new(TowerBuff.SourceType.RELIC, SOURCE_ID, AttackSpeedMultModifier.new(0.05))
+	var tower_buff = TowerBuff.new(TowerBuff.SourceType.RELIC, SOURCE_ID, AttackSpeedMultModifier.new(-0.05))
 	RunContext.towers_buffs.add_buff(tower_buff)
 
 func remove_effect() -> void:
