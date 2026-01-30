26581fd3c22e65d9d1f3a66769da1135de3639ac
CarlosribesDev <carlosribes.dev@gmail.com>
2026-01-30 14:03:39 +0100

    more hover settings

--- utils/animation_component.gd
+++ utils/animation_component.gd
@@ -1,40 +1,79 @@
 class_name AnimationComponent extends Node
 
+@export_group("Options")
 @export var from_center: bool = true
-@export var hover_scale: Vector2 = Vector2(1.1, 1.1)
-@export var time: float = 0.2
-@export var transition_type: Tween.TransitionType = Tween.TransitionType.TRANS_LINEAR
+@export var parallel_animations: bool = true
+@export var properties: Array[String] = [
+	"scale",
+	"position",
+	"rotation",
+	"size",
+	"self_modulate"
+]
+@export_group("Hover Settings")
+@export var hover_time: float = 0.2
+@export var hover_transition: Tween.TransitionType
+@export var hover_easing: Tween.EaseType = Tween.EaseType.EASE_IN_OUT
+@export var hover_scale: Vector2 = Vector2(1, 1)
+@export var hover_position: Vector2
+@export var hover_rotation: float
+@export var hover_size: Vector2
+@export var hover_modulate: Color = Color.WHITE
 
 var target: Control
 var default_scale: Vector2
+var hover_values: Dictionary
+var default_values: Dictionary
 
 
 func _ready() -> void:
 	target = get_parent() as Control
-	connect_signals()
 	call_deferred("setup")
 
-
 func connect_signals() -> void:
-	target.mouse_entered.connect(on_hovered)
-	target.mouse_exited.connect(off_hovered)
+	target.mouse_entered.connect(add_tween.bind(
+		hover_values,
+		parallel_animations,
+		hover_time,
+		hover_transition,
+		hover_easing,
+	))
+
+	target.mouse_exited.connect(add_tween.bind(
+		default_values,
+		parallel_animations,
+		hover_time,
+		hover_transition,
+		hover_easing,
+	))
 
 func setup() -> void:
 	if from_center:
 		target.pivot_offset = target.size / 2
 	default_scale = target.scale
+	default_values = {
+		"scale": target.scale,
+		"position": target.position,
+		"rotation": target.rotation,
+		"size": target.size,
+		"self_modulate": target.self_modulate,
+	}
+	hover_values = {
+		"scale": hover_scale,
+		"position": target.position + hover_position,
+		"rotation": target.rotation + deg_to_rad(hover_rotation),
+		"size": target.size * hover_size,
+		"self_modulate": hover_modulate,
+	}
+	connect_signals()
 
-func on_hovered() -> void:
-	add_tween("scale", hover_scale, time)
-
-func off_hovered() -> void:
-	add_tween("scale", default_scale, time)
-
-func add_tween(property: String, value: Variant, seconds: float) -> void:
+func add_tween(
+	values: Dictionary, 
+	parallel: bool, 
+	seconds: float, 
+	transition: Tween.TransitionType, 
+	easing: Tween.EaseType) -> void:
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(
-		target,
-		property,
-		value,
-		seconds
-	).set_trans(transition_type)
+	tween.set_parallel(parallel)
+	for property in properties:
+		tween.tween_property(target, str(property), values[property], seconds).set_trans(transition).set_ease(easing)
