4e7f00a1c1bbff0a0de2e9f08fd07053bbfffaa6
CarlosribesDev <carlosribes.dev@gmail.com>
2026-02-06 14:46:00 +0100

    check grid_positions

--- levels/procedural_test/world_map.gd
+++ levels/procedural_test/world_map.gd
@@ -3,32 +3,51 @@ class_name WorldMap extends Node2D
 @export var init_map_piece_data: MapPieceData
 @export var enemy: EnemyProcedural
 
+
 var map_pieces: Array[MapPieceData] = []
-var next_piece_to_attach: MapPiece = null
+var last_piece_attached: MapPiece = null
+#grid
+var gird_offsets: Dictionary[MapPiece.Dir, Vector2i] = {
+	MapPiece.Dir.NE: Vector2i(1, -1),
+	MapPiece.Dir.SE: Vector2i(1, 0),
+	MapPiece.Dir.SW: Vector2i(-1, 1),
+	MapPiece.Dir.NW: Vector2i(-1, 0)
+}
+var all_dirs: Array[MapPiece.Dir] = [MapPiece.Dir.NE, MapPiece.Dir.SE, MapPiece.Dir.SW, MapPiece.Dir.NW]
+var grid: Dictionary[Vector2i, bool] = {}
+var current_tile: Vector2i = Vector2i.ZERO
+
 
 func _ready() -> void:
 	var init_piece: MapPiece = init_map_piece_data.get_instance()
 	map_pieces = DataLoader.get_all_map_pieces()
-
+	grid[Vector2i.ZERO] = true
+	current_tile = Vector2i.ZERO
 	add_child(init_piece)
 
-	next_piece_to_attach = init_piece
+	last_piece_attached = init_piece
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("test"):
 		attach_next_piece()
 
 func attach_next_piece() -> void:
-	if next_piece_to_attach == null:
+	if last_piece_attached == null:
 		push_error("No piece to attach")
 		return
 
 	# next piece must connect to a valid random edge of the current piece
-	var next_dir: MapPiece.Dir = next_piece_to_attach.edges.pick_random()
+	var next_dir: MapPiece.Dir = last_piece_attached.edges.pick_random()
 	var dir_to_connect = get_dir_to_connect(next_dir)
 
+	# grid
+	current_tile = _calculate_next_tile_pos(next_dir)
+	grid[current_tile] = true
+
+	var invalid_edges = get_invalid_edges(dir_to_connect)
+
 	var valid_pieces = map_pieces.filter(func(p: MapPieceData):
-		return p.edges.has(dir_to_connect)
+		return p.edges.has(dir_to_connect) and not invalid_edges.any(func(e): return p.edges.has(e))
 	)
 
 	if valid_pieces.size() == 0:
@@ -39,9 +58,10 @@ func attach_next_piece() -> void:
 	var new_piece: MapPiece = piece_data.get_instance()
 	add_child(new_piece)
 
-	attach_piece(next_piece_to_attach, new_piece, next_dir, dir_to_connect)
+	attach_piece(last_piece_attached, new_piece, next_dir, dir_to_connect)
 	new_piece.set_edge_has_connected(dir_to_connect)
-	next_piece_to_attach = new_piece
+	last_piece_attached = new_piece
+	
 
 func attach_piece(p_piece_a: MapPiece, p_piece_b: MapPiece, entry_dir: MapPiece.Dir, exit_dir: MapPiece.Dir) -> void:
 	var a_world = p_piece_a.get_edge_tile_pos(entry_dir)
@@ -65,3 +85,21 @@ func get_dir_to_connect(dir: MapPiece.Dir) -> MapPiece.Dir:
 		_:
 			push_error("Invalid direction: %s" % [dir])
 			return MapPiece.Dir.NE
+
+
+func _calculate_next_tile_pos(next_dir: MapPiece.Dir) -> Vector2i:
+	current_tile += gird_offsets[next_dir]
+	return current_tile
+
+func get_invalid_edges(dir_to_connect: MapPiece.Dir) -> Array[MapPiece.Dir]:
+
+	var dirs_check = all_dirs.duplicate().filter(func(d): return d != dir_to_connect)
+	var invalid_dirs: Array[MapPiece.Dir] = []
+	
+
+	for dir in dirs_check:
+		var check_tile = current_tile + gird_offsets[dir]
+		if grid.has(check_tile):
+			invalid_dirs.append(dir)
+	
+	return invalid_dirs
