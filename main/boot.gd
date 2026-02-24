cdfafaeb424f260c7bb22a7346751ad583241f34
CarlosribesDev <carlosribes.dev@gmail.com>
2026-02-24 22:45:24 +0100

    fix boot

--- main/boot.gd
+++ main/boot.gd
@@ -1,9 +1,11 @@
 class_name Boot extends Node
 
 const GAME = preload("uid://6vgrx5dct8h8")
+const PROCEDURAL_TEST = preload("uid://doun1k6w4e04m")
 
 func _ready() -> void:
 	GameState.reset_run()
 	RunContext.reset_run()
-	get_tree().change_scene_to_packed(GAME)
+	await get_tree().process_frame
+	get_tree().change_scene_to_packed(PROCEDURAL_TEST)
 
--- run_context/towers_manager.gd
+++ run_context/towers_manager.gd
@@ -12,54 +12,54 @@ var towers_placed: Dictionary[Tower.Type, int] = {
 }
 
 var last_tower_ids: Dictionary[String, int] = {
-    
+	
 }
 
 var towers_ids: Array[String] = []
 
 func _init() -> void:
-    ClickEvents.tower_remove_pressed.connect(tower_removed)
-    RunContext.progress.current_level_changed.connect(_on_level_changed)
+	ClickEvents.tower_remove_pressed.connect(tower_removed)
+	RunContext.progress.current_level_changed.connect(_on_level_changed)
 
 func reset_towers() -> void:
-    _update_tower_count(Tower.Type.FIRE, 0)
-    _update_tower_count(Tower.Type.LIGHTNING, 0)
-    _update_tower_count(Tower.Type.FROST, 0)
+	_update_tower_count(Tower.Type.FIRE, 0)
+	_update_tower_count(Tower.Type.LIGHTNING, 0)
+	_update_tower_count(Tower.Type.FROST, 0)
 
 # called from tower_placer to inform
 func tower_added(tower: Tower) -> void:
-    _update_tower_count(tower.type, towers_placed[tower.type] + 1)
-    tower.id = _generate_tower_id(tower)
-    tower_placed.emit(tower)
+	_update_tower_count(tower.type, towers_placed[tower.type] + 1)
+	tower.id = _generate_tower_id(tower)
+	tower_placed.emit(tower)
 
 func tower_removed(tower: Tower) -> void:
-    var type = tower.type
-    _update_tower_count(tower.type, towers_placed[tower.type] - 1)
-    towers_ids.erase(tower.id)
-    tower.queue_free()
+	var type = tower.type
+	_update_tower_count(tower.type, towers_placed[tower.type] - 1)
+	towers_ids.erase(tower.id)
+	tower.queue_free()
 
 func get_tower_count(tower_type: Tower.Type) -> int:
-    return towers_placed[tower_type]
+	return towers_placed[tower_type]
 
 func _on_level_changed(_new_level: int) -> void:
-    reset_towers()
+	reset_towers()
 
 func _update_tower_count(tower_type: Tower.Type, value: int) -> void:
-    towers_placed[tower_type] = value
-    tower_count_change.emit(tower_type, value)
+	towers_placed[tower_type] = value
+	tower_count_change.emit(tower_type, value)
 	
 
 
 func _generate_tower_id(tower: Tower) -> String:
-    var base_id = tower.type_id
-    if not last_tower_ids.has(base_id):
-        last_tower_ids[base_id] = 0
+	var base_id = tower.type_id
+	if not last_tower_ids.has(base_id):
+		last_tower_ids[base_id] = 0
 
-    var count = last_tower_ids[base_id] + 1
-    var new_id = base_id + "_" + str(count)
-    while new_id in towers_ids:
-        count += 1
-        new_id = base_id + "_" + str(count)
-    towers_ids.append(new_id)
-    last_tower_ids[base_id] = count
-    return new_id
+	var count = last_tower_ids[base_id] + 1
+	var new_id = base_id + "_" + str(count)
+	while new_id in towers_ids:
+		count += 1
+		new_id = base_id + "_" + str(count)
+	towers_ids.append(new_id)
+	last_tower_ids[base_id] = count
+	return new_id
