a9e2c75be7e7197e52f82c8458b3413937bb8dbf
Angelet-code <sagara.ad@gmail.com>
2026-03-04 16:57:27 +0100

    camera

--- main/main_camera.gd
+++ main/main_camera.gd
@@ -1,9 +1,9 @@
 class_name MainCamera extends Camera2D
 
-@export var move_speed := 400.0 
-@export var zoom_speed := 0.25
+@export var move_speed := 500.0 
+@export var zoom_speed := 0.5
 @export var min_zoom := Vector2(0.5, 0.5)
-@export var max_zoom := Vector2(2.0, 2.0)
+@export var max_zoom := Vector2(1.0, 1.0)
 @export var level: Node2D
 
 func _process(delta: float) -> void:
