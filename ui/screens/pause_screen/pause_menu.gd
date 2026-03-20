22f4454db1a97c97ceb8cedfbfae5a6ef75de951
CarlosribesDev <carlosribes.dev@gmail.com>
2026-03-20 13:06:53 +0100

    improve menu

--- ui/screens/pause_screen/pause_menu.gd
+++ ui/screens/pause_screen/pause_menu.gd
@@ -2,9 +2,20 @@ class_name PauseMenu extends Control
 
 var MAIN_MENU = load("uid://4i6kl0xurgeg")
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("exit"):
-		resume()
+@export var settings_section: Control
+@export var menu_section: Control
+
+func _ready() -> void:
+	settings_section.visible = false
+	menu_section.visible = true
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("exit"):
+		if settings_section.visible:
+			settings_section.visible = false
+			menu_section.visible = true
+		else:
+			resume()
 
 func resume():
 	get_tree().paused = false
@@ -24,3 +35,14 @@ func _on_exit_button_xarreta_pressed() -> void:
 	resume()
 	GameState.state = GameState.STATE.ON_MAIN_MENU
 	get_tree().change_scene_to_packed(MAIN_MENU)
+
+
+func _on_back_button_xarreta_pressed() -> void:
+	settings_section.visible = false
+	menu_section.visible = true
+
+
+func _on_settings_button_xarreta_pressed() -> void:
+	print("Settings button pressed")
+	settings_section.visible = true
+	menu_section.visible = false
