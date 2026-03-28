3dc7ed44b1978f9ef8769c9ec67d9ba3c44869c6
CarlosribesDev <carlosribes.dev@gmail.com>
2026-03-28 21:13:27 +0100

    remove unused tower

--- relics/implementations/crown_of_the_forgotten_king.gd
+++ relics/implementations/crown_of_the_forgotten_king.gd
@@ -1,10 +1,4 @@
 class_name CrownOfTheForgottenKing extends Relic
 
-func apply_effect() -> void:
-	RunContext.progress.current_wave_finished.connect(_on_wave_finished)
-	
-func remove_effect() -> void:
-	RunContext.progress.current_wave_finished.disconnect(_on_wave_finished)
-
-func _on_wave_finished() -> void:
+func on_wave_finished() -> void:
 	RunContext.composite_tile_map.destroy_random_buildeable_tile()
