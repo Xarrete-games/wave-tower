d79b1090cafe764b985fbb442ff3ca1ce6795cb5
CarlosribesDev <carlosribes.dev@gmail.com>
2026-03-31 00:29:10 +0200

    flower pot

--- relics/implementations/flower_pot.gd
+++ relics/implementations/flower_pot.gd
@@ -2,24 +2,5 @@ class_name FlowerPot extends Relic
 
 const HEALTH_BONUS: int = 1
 
-var _enemies_reached_target: int = 0
-
-func apply_effect() -> void:
-	RunContext.progress.current_wave_finished.connect(_on_wave_finished)
-	RunContext.progress.current_wave_changed.connect(_on_wave_change)
-	RunContext.enemy_manager.enemy_target_reached.connect(_on_enemy_target_reached)
-
-func remove_effect() -> void:
-	RunContext.progress.current_wave_finished.disconnect(_on_wave_finished)
-	RunContext.progress.current_wave_changed.disconnect(_on_wave_change)
-	RunContext.enemy_manager.enemy_target_reached.disconnect(_on_enemy_target_reached)
-
-func _on_wave_change(_wave_num: int) -> void:
-	_enemies_reached_target = 0
-
-func _on_wave_finished() -> void:
-	if _enemies_reached_target == 0:
-		RunContext.status.health += HEALTH_BONUS
-
-func _on_enemy_target_reached(_enemy: Enemy) -> void:
-	_enemies_reached_target += 1
+func on_wave_finished() -> void:
+	RunContext.status.heal(HEALTH_BONUS)
