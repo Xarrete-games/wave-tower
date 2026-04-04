1686f0a3f0ff61a3f50a78e614f97b9c29b74745
CarlosribesDev <carlosribes.dev@gmail.com>
2026-04-04 21:01:13 +0200

    fix relics

--- relics/implementations/runic_lighter.gd
+++ relics/implementations/runic_lighter.gd
@@ -2,9 +2,8 @@ class_name RunicLighter extends Relic
 
 const DAMAGE_PER_FIRE_TOWER: float = 0.1
 
-func on_damage_multiplicative(ctx: DamageContext, amount: float) -> float:
+func on_before_attack(ctx: AttackContext) -> void:
 	if ctx.source.type == Source.SourceType.DEBUFF and ctx.source.type_id == "burn_debuff":
 		var fire_towers = RunContext.towers_manager.get_tower_count(Tower.Type.FIRE)
 		var extra_mult = 1.0 + (DAMAGE_PER_FIRE_TOWER * fire_towers)
-		return amount * extra_mult
-	return amount
+		ctx.extra_multiplicative += extra_mult
--- relics/implementations/salt.gd
+++ relics/implementations/salt.gd
@@ -3,7 +3,7 @@ class_name Salt extends Relic
 const LOW_HEALTH_THRESHOLD: float = 30
 const BONUS_MULTIPLIER: float = 0.3
 
-func on_damage_multiplicative(ctx: DamageContext, amount: float) -> float:
+func on_before_damage(ctx: DamageContext) -> void:
 	if ctx.target.get_percentage_remaining_health() <= LOW_HEALTH_THRESHOLD:
-		return amount + BONUS_MULTIPLIER
-	return amount
+		ctx.extra_multiplicative += BONUS_MULTIPLIER
+
