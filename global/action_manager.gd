ab6abffe401a0432ffb19a22d6e77bd4fd433673
CarlosribesDev <carlosribes.dev@gmail.com>
2026-04-06 23:08:56 +0200

    fix action manager

--- global/action_manager.gd
+++ global/action_manager.gd
@@ -13,8 +13,10 @@ func start_action(state: ActionState, cancel_callback: Callable = func(): pass) 
 	on_action_cancel = cancel_callback
 
 func end_action() -> void:
-	if on_action_cancel != null:
-		on_action_cancel.call()
+	var callback := on_action_cancel
+	on_action_cancel = Callable()
+	if callback.is_valid():
+		callback.call()
 	current_action = ActionState.NONE
 
 func is_action_active() -> bool:
