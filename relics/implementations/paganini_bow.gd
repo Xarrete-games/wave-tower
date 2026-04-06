ea4b207f9cf797caca7bb3cf284592169cdfd4cc
CarlosribesDev <carlosribes.dev@gmail.com>
2026-04-06 21:13:29 +0200

    power gloves

--- relics/implementations/paganini_bow.gd
+++ relics/implementations/paganini_bow.gd
@@ -2,7 +2,7 @@ class_name PaganiniBow extends Relic
 
 const BUFF_ID: String = "damage_mult_buff"
 const BUFF_VALUE: int = 10
-const COUTER_THRESHOLD: int = 3
+const COUTER_THRESHOLD: int = 4
 
 func on_tower_placed(tower: Tower) -> void:
 	counter += 1
